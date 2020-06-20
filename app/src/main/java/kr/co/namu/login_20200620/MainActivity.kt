3c86bd297174af825c62eebd03e4050d51592990
jwpark97114 <jwpark97114@gmail.com>
2020-06-20 03:08:05 -0700

    로그인 화면의 초안 그림

--- app/src/main/java/kr/co/namu/login_20200620/MainActivity.kt
+++ app/src/main/java/kr/co/namu/login_20200620/MainActivity.kt
@@ -2,10 +2,21 @@ package kr.co.namu.login_20200620
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+            val inputId = idEdt.text
+            val inputPw = pwEdt.text
+            if(inputId.toString() == "admin" && inputPw.toString() == "qwer"){
+                Toast.makeText(this, "관리자입니다.", Toast.LENGTH_SHORT).show()
+
+            }
+            }
+        }
     }
-}
